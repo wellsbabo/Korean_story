1a947c55175f3fdbd05507e596a6c85631700774
wellsbabo <wellsbabo@gmail.com>
2020-06-11 16:01:56 +0900

    20200611_2

--- app/src/main/java/com/example/korean_story/SplashActivity.java
+++ app/src/main/java/com/example/korean_story/SplashActivity.java
@@ -13,11 +13,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import java.util.ArrayList;
 
@@ -33,8 +37,20 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_intro);
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if(!task.isSuccessful()){
+                    Log.w("FCM Log","getInstanceId failed", task.getException());
+                    return;
+                }
+                String token = task.getResult().getToken();
+                Log.d("FCM Log", "FCM 토큰: "+token);
+                //Toast.makeText(MainActivity.this, token,Toast.LENGTH_SHORT).show();
+            }
+        });
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
